3e4f09c1326ec0ab07b134191f721564981ca780
Antonio Ledesma <117119503+Antony2023@users.noreply.github.com>
2025-05-01 21:57:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const menu=document.querySelector(".menu");
+const openMenuBtn=document.querySelector(".abrir-menu");
+const closeMenuBtn=document.querySelector(".cerrar-menu");
+
+function toggleMenu(){
+    menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click",toggleMenu);
+closeMenuBtn.addEventListener("click",toggleMenu);
